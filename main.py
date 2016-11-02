d2ed75c4e82579ce3786b3b94e39e9f9e2039dd7
Dorian Kurzaj <doriank@hotmail.fr>
2016-11-02 15:55:02 +0100

    First comit

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == "__main__":
+    print("Veuillez entrer le chemin vers le dossier dans lequel vous souhaitez effectuer une recherche :")
+    chemin = input()
+    for nom_fichier in os.listdir(chemin):
+            print("|-" + nom_fichier)
