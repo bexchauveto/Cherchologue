31405f13744b1c3166e9d5cd3803d7717eb84e79
Dorian Kurzaj <doriank@hotmail.fr>
2016-11-20 12:29:22 +0100

    Ajout de la gestion de l'encodage et des stopwords

--- main.py
+++ main.py
@@ -4,38 +4,46 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import WhitespaceTokenizer
+from nltk.corpus import stopwords
 import re
 from collections import Counter
 import json
 from collections import OrderedDict
 from pprint import pprint
 import operator
+import chardet
+import urllib.request
 
 if __name__ == "__main__":
     tokens = []
-    for nom_fichier in os.listdir("/home/dorian/Documents/INSA/5 IL/Recherche d'Informations/RessourcesProjet/corpus-utf8"):
-        print(nom_fichier)
-        if nom_fichier != "D46.html" and nom_fichier != "D75.html" and nom_fichier != "D89.html"  and nom_fichier != "D45.html":
-            stemmer = SnowballStemmer("french")
-            soup = BeautifulSoup(open("/home/dorian/Documents/INSA/5 IL/Recherche d'Informations/RessourcesProjet/corpus-utf8/"+nom_fichier), "lxml")
+    stops = set(stopwords.words("french"))
+    for nom_fichier in os.listdir("../RessourcesProjet/corpus-utf8"):
+        with urllib.request.urlopen("file:///home/dorian/Documents/INSA/5 IL/Recherche d'Informations/RessourcesProjet/corpus-utf8/" + nom_fichier) as url:
+            rawdata = url.read()
+        encodage = chardet.detect(rawdata)["encoding"]
+        print(nom_fichier + " (" + encodage + ")")
+        stemmer = SnowballStemmer("french")
+        soup = BeautifulSoup(open("../RessourcesProjet/corpus-utf8/"+nom_fichier, encoding=encodage), "lxml", from_encoding=encodage)
 
 
-            # kill all script and style elements
-            for script in soup(["script", "style"]):
-                script.extract()    # rip it out
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
 
-            # get text
-            text = soup.get_text()
+        # get text
+        text = soup.get_text()
 
-            # break into lines and remove leading and trailing space on each
-            lines = (line.strip() for line in text.splitlines())
-            # break multi-headlines into a line each
-            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            # drop blank lines
-            text = '\n'.join(chunk for chunk in chunks if chunk)
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
 
-            tokens += WhitespaceTokenizer().tokenize(text)
-            print(tokens)
+        tokens += WhitespaceTokenizer().tokenize(text)
+        # We remove the stop words
+        tokens = [token for token in tokens if token not in stops]
+        # print(tokens)
 
     nonPunct = re.compile('.*[A-Za-z].*')  # must contain a letter or digit
     filtered = [w for w in tokens if nonPunct.match(w)]
