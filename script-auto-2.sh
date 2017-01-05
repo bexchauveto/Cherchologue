3cedcc9ea05a202d7d3ac05da595b857676e0da2
Bex-Chauvet <olivier.bexchauvet@gmail.com>
2017-01-05 20:18:01 +0100

    ajout du script 2

---
+++ script-auto-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./main.py 1 tfn-idf-ps
+./main.py 1 tfn-idf-cd
+./main.py 1 tfn-idf-cos
+./main.py 1 tfn-idf-jac
+./main.py 1 tfr-idf-ps
+./main.py 1 tfr-idf-cd
+./main.py 1 tfr-idf-cos
+./main.py 1 tfr-idf-jac
